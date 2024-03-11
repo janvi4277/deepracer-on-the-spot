53a638ef6e5ab383a4991602b0eb9681692ce6a4
janvi1707 <janvi.uppalapati42@gmail.com>
2024-03-11 22:00:01 +0530

    pp5

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -20,7 +20,7 @@ def reward_function(params):
     straight_waypoints = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169];
     left_waypoints=[93,94,95,96,97,98,99,100,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,117,118,119,120,121,132,133,134,135]
     right_waypoints=[77,78,79,80,81,82,83]
-    not_very_right_waypoints=[67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84]
+    not_very_right_waypoints=[67,68,69,70,71,72,73,74,75,76]
     not_very_left=[122, 123, 124, 125, 126, 127, 128, 129, 130, 131]
     # Calculate the direction of the center line based on the closest waypoints
     waypoints_length= len(waypoints)
