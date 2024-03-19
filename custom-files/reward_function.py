2b8ea458bfefd2e04dff729184f4b7a35321eafb
janvi1707 <janvi.uppalapati42@gmail.com>
2024-03-19 11:34:41 +0530

    k9

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -41,6 +41,7 @@ def reward_function(params):
     basic_left=[18,19,20,21,22,23,53,54,55,56,57,58,59,60,89,90,91,92,101,102,103,104,105,106,107,1108,109,110,111,112,113,114,115,116,136,137,138,139,140,141,142,143]
     basic_right=[61,62,63,64,65,66,84,85,86,87,88]
     curve_points=[122, 123, 124, 125, 126, 127, 128, 129, 130, 131,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,93,94,95,96,97,98,99,100,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,117,118,119,120,121,132,133,134,135]
+    turn_points = [23,24,25,26,27,28,29,30,31,46,47,48,49,50,51,52,92,93,94,95,96,97,98,99,100,101,116,117,118,119,120,132,133,134,135,136,137]
     # Calculate the direction of the center line based on the closest waypoints
     waypoints_length= len(waypoints)
     prev = int(closest_waypoints[0])
@@ -85,8 +86,11 @@ def reward_function(params):
 
     if next in straight_waypoints:
         steering_reward = 200/(1+abs(straight_direction_diff - total_angle))
+    elif next in turn_points:
+        steering_reward = 300/(1+abs(params['steering_angle']-total_angle))
     else:
-        steering_reward = 200/(1+abs(params['steering_angle']-total_angle))
+        steering_reward = 100/(1+abs(params['steering_angle']-total_angle))
+
     if abs(total_angle) >30 and abs(params['steering_angle'])>25 and total_angle*params['steering_angle']>=0:
         steering_reward=100
     if params['steps'] > 0:
@@ -101,8 +105,12 @@ def reward_function(params):
     if abs(total_angle)<=5:
         if params['speed'] >=3:
             speed_reward+=30
+        if params['speed'] >=3.2:
+            speed_reward+=30
         if params['speed'] >=3.4:
-            speed_reward+=40
+            speed_reward+=30
+        if params['speed'] >=3.6:
+            speed_reward+=30
         if params['speed'] >=3.8:
             speed_reward+=50
         if params['speed'] >=4:
